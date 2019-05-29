a318eae15a658a43503dc625efa3c48eee135db1
Tanmai H <vvijaytanmai@gmail.com>
2019-05-29 12:28:05 +0530

    works without NaN problem

--- lista.py
+++ lista.py
@@ -7,9 +7,9 @@ def soft_threshold(x, threshold, iter=-1):
     #sign(x) * max(0, |x| -T)
     return tf.multiply(tf.sign(x), tf.maximum(tf.abs(x) - threshold, 0), name="Z_{}".format(iter))    
 
-def get_data(n_samples=100, n_features=30, n_components=50, n_nonzero_coefs=7):
+def get_data(n_samples=100, n_features=30, n_components=50, n_nonzero_coefs=7, random_state=None):
     Y,A,X = make_sparse_coded_signal(n_samples=n_samples,n_features=n_features,n_components=n_components
-                                    ,n_nonzero_coefs=n_nonzero_coefs)
+                                    ,n_nonzero_coefs=n_nonzero_coefs, random_state=random_state)
     #AX = Y
     return Y,A,X                                
 
@@ -25,17 +25,17 @@ class Lista:
         self.model_path = model_path
 
         self.x = tf.placeholder(dtype=tf.float32, shape=(None, n_features), name='input')
+        self.zmin = tf.placeholder(dtype=tf.float32, shape=(None, n_components), name='optimal_output')
+        
         rand_mat = np.random.uniform(size=(n_components, n_features))
-        print("AAAAAAAAAAAAAAAA rand_mat = ", rand_mat)
         self.We = tf.Variable(rand_mat, dtype=tf.float32, name='We')                                                            
         
-        s,_,_ = tf.svd(tf.matmul(self.We, tf.transpose(self.We)), name='svd_op')
-        self.Lipschitz = tf.reduce_max(s) ** 2
+        e,_ = tf.linalg.eigh(tf.matmul(self.We, tf.transpose(self.We)), name='eigen_op') #tf.svd(tf.matmul(self.We, tf.transpose(self.We)), name='svd_op')
+        self.Lipschitz = tf.reduce_max(e)
 
         self.B = tf.matmul(self.We, tf.transpose(self.x), name='B')        
         self.S = tf.eye(self.M) - 1/self.Lipschitz * (tf.matmul(self.We, tf.transpose(self.We), name="inside_S"))
         self.theta = self._lambda / 2 / self.Lipschitz
-        #self.Z = tf.Variable(np.zeros(shape=(n_components, 1)),  dtype=tf.float32, name='Z')
         self.Z_list = []
 
         print('We:',  self.We.shape)
@@ -44,7 +44,7 @@ class Lista:
         print('B: ',  self.B.shape)
         print('S: ',  self.S.shape)
 
-        old_Z = tf.zeros((n_components, 1), tf.float32)
+        old_Z = tf.zeros((n_components, None), tf.float32)
 
         for i in range(T):
             Z = activation(self.B + tf.matmul(self.S, old_Z), self.theta, i)
@@ -60,33 +60,44 @@ class Lista:
         self.writer = tf.summary.FileWriter(tb_path + model_name)
         
 
-        self.loss = tf.reduce_mean(tf.nn.l2_loss(self.Z - tf.matmul(self.We, tf.transpose(self.x))) + tf.norm(self.Z, ord=1))
-        tf.summary.scalar('f_loss: ', self.loss)
+        self.loss = tf.reduce_mean(tf.nn.l2_loss(self.Z - tf.transpose(self.zmin)))
+        tf.summary.scalar('l2_loss: ', self.loss)
         
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
         
         self.merged_summary = tf.summary.merge_all()
         with tf.Session() as sess:
             self.writer.add_graph(sess.graph)
-        
+                   
 
-    def learn(self, x_train, max_iter=100):
+    def learn(self, x_train, y_train, max_iter=100):
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
             for i in range(max_iter):
-                loss, _, B, S, theta, lips = sess.run([self.loss, self.optimizer, self.We, self.S, self.theta, self.Lipschitz], feed_dict = {self.x: x_train})
-                
-                #summary = sess.run([self.merged_summary], feed_dict={self.x: x_train})
-                print("Epoch", i, "/",max_iter, " Loss = ", loss)
-                print("lipschitz = ", lips, "We = ", B, "S = ", S, "theta = ", theta)
+                loss, _, theta, lips,summary = sess.run([self.loss, self.optimizer, self.theta, self.Lipschitz, self.merged_summary], feed_dict = {self.x: x_train, self.zmin: y_train})
+                # summary = sess.run([self.merged_summary], feed_dict={self.x: x_train})
+                self.writer.add_summary(summary, i)
+                print("Epoch", i, "/",max_iter, " Loss = ", loss, "lipschitz: ", lips, " theta: ", theta)
+    
+    def predict(self, x_train, z0):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            z = sess.run([self.Z], feed_dict={self.x: x_train})
+            z=z[0]
+            print(z)
+            print(z0)
+            print(np.linalg.norm(z-z0, ord=2))
+    
 def main():
-    X,A,Z = get_data(n_features=30, n_components=50)
+    X,A,Z = get_data(n_samples=100, n_features=30, n_components=50)
     # X = AZ
     N,M = A.shape
     
-    model = Lista(n_features=N, n_components=M)
-    model.learn(x_train=X.T, max_iter=20)
-
+    model = Lista(n_features=N, n_components=M,T=6)
+    model.learn(x_train=X.T, y_train= Z.T, max_iter=2000)
+    
+    x,a,z = X[:, 1], None, Z[:, 1]
+    model.predict(x_train=np.reshape(x.T, (1, x.shape[0])), z0=z)
 if __name__ == '__main__':
     main()
